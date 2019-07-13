fc1b02e703b7a108b0ff65c5faf025fe69f22eae
Hippolyte SOVI <sovhip@gmail.com>
2019-07-13 17:25:55 +0000

    Mise en place de la structure de l'application (modules, components, services)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BackendModule } from './backend/backend.module';
+import { FrontendModule } from './frontend/frontend.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BackendModule,
+    FrontendModule
   ],
   providers: [],
   bootstrap: [AppComponent]
